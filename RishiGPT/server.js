ac790e85d90f5d01926d92d7e158507af49887e2
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-05-10 14:00:05 +0530

    Update server.js

--- RishiGPT/server.js
+++ RishiGPT/server.js
@@ -24,26 +24,14 @@ app.post("/userdata", (req, res) => {
   const openai = new OpenAIApi(configuration);
   async function main(ques) {
     var message = { body: ques };
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: "Your name is Ava. You are a helpful assistant.",
-        },
-        {
-          role: "user",
-          content:
-            "Assume you are a Hindu saint from old times and posess knowledge of hindu scriptures like the Veda,Gita,Mahabharata,Ramayana and many more. You would call be as My Child and answer this question:" +
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+     prompt:  "Assume you are a Hindu saint from old times and posess knowledge of hindu scriptures like the Veda,Gita,Mahabharata,Ramayana and many more. You would call be as My Child and answer this question:" +
             ques +
-            "If your response is small add some small quotations of about 100words from any scriptures and make the complete response about 190 words and don't have unfinished sentences",
-        },
-      ],
+            "If your response is small add some small quotations of about 100words from any scriptures and make the complete response about 190 words and don't have unfinished sentences"
       temperature: 0.2,
       max_tokens: 300,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
+      
     });
     reqCount += 1;
 
