a232c74988ffdd4ff5a5f002c5dc32a1e15c0455
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-05-15 12:06:53 +0530

    Update server.js

--- RishiGPT/server.js
+++ RishiGPT/server.js
@@ -22,20 +22,31 @@ app.post("/userdata", (req, res) => {
   var question = req.body.query;
 
   const openai = new OpenAIApi(configuration);
-  async function main() {
-  const openai = new OpenAIApi(configuration);
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt:
-      "Assume you are a Hindu saint from old times and posess knowledge of hindu scriptures like the Veda,Gita,Mahabharata,Ramayana and many more. You would call be as My Child and answer this question: " +
-      "Who are you?",
-    max_tokens: 200,
-  });
-    reqCount += 1;
-
-    console.log(reqCount + ") User Asked: " + ques);
-
+  async function main(ques) {
+    var message = { body: ques };
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "Your name is Ava. You are a helpful assistant.",
+        },
+        {
+          role: "user",
+          content:
+            "Assume you are a Hindu saint from old times and posess knowledge of hindu scriptures like the Veda,Gita,Mahabharata,Ramayana and many more. You would call be as My Child and answer this question:" +
+            question +"Make your answer about 150 words and don't have any unfinished sentences",
+        },
+      ],
+      temperature: 0.2,
+      max_tokens: 200,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
     res.send(completion.data.choices[0].message.content);
+    req+=1;
+    console.log(req+question);
   }
 
   main(question);
