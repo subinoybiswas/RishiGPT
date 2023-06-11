04c0be1a0e30280bc195cb2e2dec52b497e577f3
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-06-12 00:13:14 +0530

    Update work.js

--- RishiGPT/src/work.js
+++ RishiGPT/src/work.js
@@ -5,7 +5,7 @@ var query = document.getElementById("query").value;
 var loading = document.getElementsByClassName("loader");
 
 function showError(){
-alert("Rishi is resting now");
+alert("Rishi is resting now. Try again later");
   loading[0].hidden = true;
 }
 
@@ -14,15 +14,17 @@ console.log(query);
 xhr.onloadstart = () => {
   loading[0].hidden = false;
   loading[0].scrollIntoView();
+  setTimeout( function() { showError(); }, 5000);
 };
 xhr.onload = function () {
   if (xhr.status === 200) {
     console.log(xhr.responseText);
     resp[0].innerHTML = xhr.responseText;
     search1.scrollIntoView();
-    setTimeout( function() { showError(); }, 10000);
+    
   } else {
     console.log("Request failed. Returned status: " + xhr.status);
+    
   }
 };
 xhr.onloadend = () => {
