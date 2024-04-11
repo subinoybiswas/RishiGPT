010e4efec89d67d9cf4438572e2d60f999f9fb7a
Subinoy Biswas <heysubinoy@gmail.com>
2024-04-11 16:09:28 +0530

    host: vercel host try two

--- server.js
+++ server.js
@@ -1,90 +1,43 @@
 const express = require("express");
 const app = express();
-// require("dotenv").config();
-// const { Configuration, OpenAIApi } = require("openai");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-let reqCount = 0;
-PORT = process.env.PORT || 8080;
-app.set("trust proxy", true);
-console.log(process.env.BARD_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.BARD_API_KEY);
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("./src"));
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log("Application started and Listening on port", PORT);
 });
-//HTML-CSS
-app.use(express.static(__dirname + "/src"));
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/src/index.html");
-});
-//HTML-CSS
-// const configuration = new Configuration({
-//   apiKey: process.env.MY_API_KEY,
-// }); //legacy OpenAI Code
-var qno = 0;
-console.log(process.env.BARD_API_KEY);
-const genAI = new GoogleGenerativeAI(process.env.BARD_API_KEY);
 
 app.post("/userdata", async (req, res) => {
   try {
-    var question = req.body.query;
+    const question = req.body.query;
+    const prompt =
+      "Assume you are a Hindu saint from old times and possess knowledge of Hindu scriptures like the Veda, Gita, Mahabharata, Ramayana, and many more. You would call me 'My Child' and answer this question wrapping it in about 100 words: " +
+      question;
 
-    // const openai = new OpenAIApi(configuration);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-pro",
+    });
 
-    async function main(ques) {
-      var message = { body: ques };
-      // const completion = await openai.createChatCompletion({
-      //   model: "gpt-3.5-turbo",
-      //   messages: [
-      //     {
-      //       role: "user",
-      //       content:
-      //         "Imagine you're a wise Hindu saint from ancient times, well-versed in scriptures like the Vedas, Gita, Mahabharata, and Ramayana. Address me as 'My Child' wrap your answer in 100 words. Limit your wisdom to matters of religion; disregard modern queries as beyond your era of knowledge (1000-2000 years ago). My question is this: " +
-      //         question,
-      //     },
-      //   ],
-      //   temperature: 0.2,
-      //   max_tokens: 200,
-      //   top_p: 1,
-      //   frequency_penalty: 0,
-      //   presence_penalty: 0,
-      // });
-      const model = genAI.getGenerativeModel({
-        model: "gemini-pro",
-      });
+    const result = await model.generateContent(prompt);
+    const response = result.response;
 
-      const prompt =
-        "Assume you are a Hindu saint from old times and posess knowledge of hindu scriptures like the Veda,Gita,Mahabharata,Ramayana and many more. You would call be as My Child and answer this question wraping in about 100 words: " +
-        question;
-
-      const result = await model.generateContent(prompt);
-      const response = result.response;
-      if (response.promptFeedback.blockReason) {
-        res.send(
-          "My child, I am angred by this question! Do not ask me these kind of questions again!"
-        );
-        return;
-      }
-      const text = response.text();
-      res.send(text);
-      qno += 1;
-      console.log(qno, question);
+    if (response.promptFeedback.blockReason) {
+      res.send(
+        "My child, I am angered by this question! Do not ask me these kinds of questions again!"
+      );
       return;
     }
 
-    main(question);
+    const text = response.text();
+    res.send(text);
+    console.log("Question:", question);
   } catch (error) {
-    // Handle the error here, you can log it or send an error response to the client.
-    console.error("Error:");
+    console.error("Error:", error);
     res.status(500).send("Internal Server Error");
   }
 });
-
-//createCompletion({
-//   model: "text-davinci-003",
-//   prompt:
-//     "Assume you are a Hindu saint from old times and posess knowledge of hindu scriptures like the Veda,Gita,Mahabharata,Ramayana and many more. You would call be as My Child and answer this question: " +
-//     ques,
-//   max_tokens: 200,
-// });
