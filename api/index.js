254f8078966f17db9c75ea2a823c17c4452b4f69
Subinoy Biswas <heysubinoy@gmail.com>
2024-04-11 21:33:01 +0530

    configure: for vercel

--- api/index.js
+++ api/index.js
@@ -1,27 +1,18 @@
-const express = require("express");
-const app = express();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.BARD_API_KEY);
-const PORT = process.env.PORT || 8080;
 
-app.use(express.static("src"));
-app.use(express.json());
-
-app.listen(PORT, () => {
-  console.log("Application started and Listening on port", PORT);
-});
-
-app.get("*", (req, res) => {
-  res.sendFile("index.html", { root: "src" });
-});
-
-app.post("/userdata", async (req, res) => {
+module.exports = async (req, res) => {
   try {
-    const question = req.body.query;
+    if (req.method !== "POST") {
+      // Reject non-POST requests
+      return res.status(405).send({ error: "Method Not Allowed" });
+    }
+
+    const { query } = req.body;
     const prompt =
       "Assume you are a Hindu saint from old times and possess knowledge of Hindu scriptures like the Veda, Gita, Mahabharata, Ramayana, and many more. You would call me 'My Child' and answer this question wrapping it in about 100 words: " +
-      question;
+      query;
 
     const model = genAI.getGenerativeModel({
       model: "gemini-pro",
@@ -30,18 +21,19 @@ app.post("/userdata", async (req, res) => {
     const result = await model.generateContent(prompt);
     const response = result.response;
 
-    if (response.promptFeedback.blockReason) {
-      res.send(
-        "My child, I am angered by this question! Do not ask me these kinds of questions again!"
-      );
-      return;
+    if (response.promptFeedback?.blockReason) {
+      return res
+        .status(400)
+        .send(
+          "My child, I am angered by this question! Do not ask me these kinds of questions again!"
+        );
     }
 
     const text = response.text();
-    res.send(text);
-    console.log("Question:", question);
+    res.status(200).send(text);
+    console.log("Question:", query);
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Internal Server Error");
   }
-});
+};
